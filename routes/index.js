7c55e9f996ad290c91319fa3406c743715d3e511
kunal-relan <kunal@loudshout.in>
2015-10-04 02:16:15 +0530

    This is a test app
    :) just doing some testing

---
+++ routes/index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+  res.render('index',{
+      title : 'Expressss',
+      todos : [
+        {
+          description : "Hy I am just testing the app",
+          due : new Date(new Date().getTime() + 24 * 60 * 60 * 1000),
+          done :false
+        },
+        {
+          description : "something more",
+          due : new Date(new Date().getTime() + 24 * 60 * 60 * 1000),
+          done : true
+
+        },
+
+      ]
+
+
+
+    });
+});
+
+module.exports = router;
